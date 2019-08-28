6865d28449335a2296e4e37cdc22b42c95063703
许助云 <xuzhuyun@fcbox.com>
2019-08-28 11:27:31 +0800

    feat: 更新  VideoUtils 工具类。

--- lib_util/src/main/java/com/xzy/utils/video/VideoUtils.java
+++ lib_util/src/main/java/com/xzy/utils/video/VideoUtils.java
@@ -1,9 +1,220 @@
 package com.xzy.utils.video;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.media.MediaMetadataRetriever;
+import android.provider.MediaStore;
+
+import static android.media.ThumbnailUtils.OPTIONS_RECYCLE_INPUT;
+
 /**
  * 视频处理相关工具类。
+ * 部分参考 https://blog.csdn.net/ouyang_peng/article/details/16864975
+ *
+ * @author xzy
  */
+@SuppressWarnings("unused")
 public class VideoUtils {
 
+    /**
+     * Constant used to indicate the dimension of micro thumbnail.
+     *
+     * @hide Only used by media framework and media provider internally.
+     */
+    private static final int TARGET_SIZE_MICRO_THUMBNAIL = 96;
+
+    /* Options used internally. */
+    private static final int OPTIONS_NONE = 0x0;
+
+    private static final int OPTIONS_SCALE_UP = 0x1;
+
+    /**
+     * Create a video thumbnail for a video. May return null if the video is
+     * corrupt or the format is not supported. 创建一张视频的缩略图
+     *
+     * @param filePath the path of video file
+     * @param kind     could be MINI_KIND or MICRO_KIND
+     */
+    public static Bitmap createVideoThumbnail(String filePath, int kind) {
+        Bitmap bitmap = null;
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        try {
+            retriever.setDataSource(filePath);
+            bitmap = retriever.getFrameAtTime(-1);
+        } catch (IllegalArgumentException ex) {
+            // Assume this is a corrupt video file
+        } catch (RuntimeException ex) {
+            // Assume this is a corrupt video file.
+        } finally {
+            try {
+                retriever.release();
+            } catch (RuntimeException ex) {
+                // Ignore failures while cleaning up.
+            }
+        }
+
+        if (bitmap == null) return null;
+
+        if (kind == MediaStore.Images.Thumbnails.MINI_KIND) {
+            // Scale down the bitmap if it's too large.
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            int max = Math.max(width, height);
+            if (max > 512) {
+                float scale = 512f / max;
+                int w = Math.round(scale * width);
+                int h = Math.round(scale * height);
+                bitmap = Bitmap.createScaledBitmap(bitmap, w, h, true);
+            }
+        } else if (kind == MediaStore.Images.Thumbnails.MICRO_KIND) {
+            bitmap = extractThumbnail(bitmap,
+                    TARGET_SIZE_MICRO_THUMBNAIL,
+                    TARGET_SIZE_MICRO_THUMBNAIL,
+                    OPTIONS_RECYCLE_INPUT);
+        }
+        return bitmap;
+    }
+
+    /**
+     * Creates a centered bitmap of the desired size. 创建一个指定大小的缩略图
+     *
+     * @param source original bitmap source
+     * @param width  targeted width
+     * @param height targeted height
+     */
+    public static Bitmap extractThumbnail(
+            Bitmap source, int width, int height) {
+        return extractThumbnail(source, width, height, OPTIONS_NONE);
+    }
+
+    /**
+     * Creates a centered bitmap of the desired size.
+     *
+     * @param source  original bitmap source
+     * @param width   targeted width
+     * @param height  targeted height
+     * @param options options used during thumbnail extraction
+     *                表示在缩略图抽取时提供的选项，如果options定义为OPTIONS_RECYCLE_INPUT,
+     *                则回收@param source这个资源文件(除非缩略图等于@param source)
+     */
+    public static Bitmap extractThumbnail(
+            Bitmap source, int width, int height, int options) {
+        if (source == null) {
+            return null;
+        }
+
+        float scale;
+        if (source.getWidth() < source.getHeight()) {
+            scale = width / (float) source.getWidth();
+        } else {
+            scale = height / (float) source.getHeight();
+        }
+        Matrix matrix = new Matrix();
+        matrix.setScale(scale, scale);
+        return transform(matrix, source, width, height,
+                OPTIONS_SCALE_UP | options);
+    }
+
+
+    /**
+     * Transform source Bitmap to targeted width and height.
+     */
+    private static Bitmap transform(Matrix scaler,
+                                    Bitmap source,
+                                    int targetWidth,
+                                    int targetHeight,
+                                    int options) {
+        boolean scaleUp = (options & OPTIONS_SCALE_UP) != 0;
+        boolean recycle = (options & OPTIONS_RECYCLE_INPUT) != 0;
+
+        int deltaX = source.getWidth() - targetWidth;
+        int deltaY = source.getHeight() - targetHeight;
+        if (!scaleUp && (deltaX < 0 || deltaY < 0)) {
+            /*
+             * In this case the bitmap is smaller, at least in one dimension,
+             * than the target.  Transform it by placing as much of the image
+             * as possible into the target and leaving the top/bottom or
+             * left/right (or both) black.
+             */
+            Bitmap b2 = Bitmap.createBitmap(targetWidth, targetHeight,
+                    Bitmap.Config.ARGB_8888);
+            Canvas c = new Canvas(b2);
+
+            int deltaXHalf = Math.max(0, deltaX / 2);
+            int deltaYHalf = Math.max(0, deltaY / 2);
+            Rect src = new Rect(
+                    deltaXHalf,
+                    deltaYHalf,
+                    deltaXHalf + Math.min(targetWidth, source.getWidth()),
+                    deltaYHalf + Math.min(targetHeight, source.getHeight()));
+            int dstX = (targetWidth - src.width()) / 2;
+            int dstY = (targetHeight - src.height()) / 2;
+            Rect dst = new Rect(
+                    dstX,
+                    dstY,
+                    targetWidth - dstX,
+                    targetHeight - dstY);
+            c.drawBitmap(source, src, dst, null);
+            if (recycle) {
+                source.recycle();
+            }
+            c.setBitmap(null);
+            return b2;
+        }
+        float bitmapWidthF = source.getWidth();
+        float bitmapHeightF = source.getHeight();
+
+        float bitmapAspect = bitmapWidthF / bitmapHeightF;
+        float viewAspect = (float) targetWidth / targetHeight;
+
+        if (bitmapAspect > viewAspect) {
+            float scale = targetHeight / bitmapHeightF;
+            if (scale < .9F || scale > 1F) {
+                scaler.setScale(scale, scale);
+            } else {
+                scaler = null;
+            }
+        } else {
+            float scale = targetWidth / bitmapWidthF;
+            if (scale < .9F || scale > 1F) {
+                scaler.setScale(scale, scale);
+            } else {
+                scaler = null;
+            }
+        }
+
+        Bitmap b1;
+        if (scaler != null) {
+            // this is used for minithumb and crop, so we want to filter here.
+            b1 = Bitmap.createBitmap(source, 0, 0,
+                    source.getWidth(), source.getHeight(), scaler, true);
+        } else {
+            b1 = source;
+        }
+
+        if (recycle && b1 != source) {
+            source.recycle();
+        }
+
+        int dx1 = Math.max(0, b1.getWidth() - targetWidth);
+        int dy1 = Math.max(0, b1.getHeight() - targetHeight);
+
+        Bitmap b2 = Bitmap.createBitmap(
+                b1,
+                dx1 / 2,
+                dy1 / 2,
+                targetWidth,
+                targetHeight);
+
+        if (b2 != b1) {
+            if (recycle || b1 != source) {
+                b1.recycle();
+            }
+        }
+
+        return b2;
+    }
 
 }
